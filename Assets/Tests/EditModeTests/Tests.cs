aedf08fd4ae08b2fe37eab64d08170150a167791
Daria Karamova <d.karamova@crazypanda.ru>
2023-03-12 19:37:09 +0300

    test workflow

---
+++ Assets/Tests/EditModeTests/Tests.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+public class Tests
+{
+    [Test]
+    public void TestsSimplePasses()
+    {
+        Assert.Pass();
+    }
+}
